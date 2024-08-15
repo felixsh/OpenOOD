accbe948e7a3921a117fac6a77086771dd2b38a7
Felix Hauser <felix.hauser@kit.edu>
2024-08-15 18:11:43 +0200

    Switch NECO postprocessor to PC decomposition

--- openood/postprocessors/neco_postprocessor.py
+++ openood/postprocessors/neco_postprocessor.py
@@ -1,7 +1,6 @@
 from typing import Any
 
-import numpy as np
-from sklearn.decomposition import PCA
+from nc_toolbox import principal_decomp
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -14,14 +13,16 @@ class NECOPostprocessor(BasePostprocessor):
         super(NECOPostprocessor, self).__init__(config)
         self.args = self.config.postprocessor.postprocessor_args
         print('postprocessor.args:', self.args)
-        self.d = self.args.d
-        self.pca = None
+        self.D = self.args.D
+        self.center = self.args.center
+        self.P = None
+        self.mean = None
         self.args_dict = self.config.postprocessor.postprocessor_sweep
         self.setup_flag = False
 
     def setup(self, net: nn.Module, id_loader_dict, ood_loader_dict):
         if not self.setup_flag:
-            h = []
+            H = []
             net.eval()
             with torch.no_grad():
                 for batch in tqdm(id_loader_dict['train'],
@@ -32,11 +33,16 @@ class NECOPostprocessor(BasePostprocessor):
                     data = data.float()
 
                     _, feature = net(data, return_feature=True)
-                    h.append(feature.data.cpu().numpy())
+                    H.append(feature.data)
 
-            h = np.concatenate(h, axis=0)
-            self.pca = PCA(n_components=self.d)
-            self.pca.fit(h)
+            H = torch.cat(H, dim=0).cpu().numpy()
+
+            if self.center:
+                P, _, mean = principal_decomp(H, n_components=self.D, center=True)
+                self.mean = torch.as_tensor(mean).cuda()  # (d,)
+            else:
+                P, _ = principal_decomp(H, n_components=self.D, center=False)
+            self.P = torch.as_tensor(P).cuda()  # (D, d)
 
             self.setup_flag = True
         else:
@@ -47,13 +53,17 @@ class NECOPostprocessor(BasePostprocessor):
         logits, features = net.forward(data, return_feature=True)  # (b x c), (b x d)
         _, preds = torch.max(logits, dim=1)
 
-        features_transformed = torch.as_tensor(self.pca.transform(features.cpu().numpy())).cuda()
-        scores = torch.norm(features_transformed, dim=1) / torch.norm(features, dim=1)
+        # (b x d) @ (d, D) = (b x D)
+        if self.center:
+            features_transformed = torch.matmul(features - self.mean, self.P.T)
+        else:
+            features_transformed = torch.matmul(features, self.P.T)
+        scores = torch.norm(features_transformed, dim=1) / torch.norm(features, dim=1)  # (b,)
 
         return preds, scores
 
     def set_hyperparam(self, hyperparam: list):
-        self.d = hyperparam[0]
+        self.D = hyperparam[0]
 
     def get_hyperparam(self):
-        return self.d
+        return self.D
