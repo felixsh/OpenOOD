755a3a23d1af61d8d12d67762a672ad25b00b1ae
Felix Hauser <felix.hauser@kit.edu>
2024-11-01 21:34:51 +0100

    Implement ECDF combining of scores

--- openood/postprocessors/my_postprocessor.py
+++ openood/postprocessors/my_postprocessor.py
@@ -2,6 +2,7 @@ from typing import Any
 
 import numpy as np
 from sklearn.covariance import EmpiricalCovariance, MinCovDet
+from statsmodels.nonparametric.kernel_density import KDEMultivariate
 import torch
 import torch.nn as nn
 from tqdm import tqdm
@@ -10,6 +11,7 @@ import nc_toolbox as nctb
 
 from .base_postprocessor import BasePostprocessor
 
+import matplotlib.pyplot as plt
 
 class MyPostprocessor(BasePostprocessor):
     def __init__(self, config):
@@ -45,29 +47,43 @@ class MyPostprocessor(BasePostprocessor):
             h_split = nctb.split_embeddings(h, l)
 
             estimator = EmpiricalCovariance
-            # estimator = MinCovDet
+            estimator = MinCovDet
             self.covars = [estimator().fit(x) for x in h_split.values()]
-# 
+
+            cluster_scores = self._cluster_score(h)
+            dist_scores = self._dist_score(h)
+            data = self._concat((cluster_scores, dist_scores))
+            self.data_kde = KDEMultivariate(data, var_type='cc')
+
             self.setup_flag = True
         else:
             pass
 
+    def _concat(self, arrays1d):
+        return np.concatenate([a.reshape(-1, 1) for a in arrays1d], axis=1)
+
+    def _cluster_score(self, features):
+        dists = [cov.mahalanobis(features) for cov in self.covars]
+        dists = self._concat(dists)  # b x c
+        scores = np.max(-dists, axis=1)  # b x 1, higher is better
+        return scores
+
+    def _dist_score(self, features):
+        return np.linalg.norm(features, axis=1, ord=2)  # higher is better
+
     @torch.no_grad()
     def postprocess(self, net: nn.Module, data: Any):
         logits, features = net.forward(data, return_feature=True)
-
-        # Cluster score
+        
         features = features.detach().cpu().numpy()
-        dists = [cov.mahalanobis(features) for cov in self.covars]
-        dists = [d.reshape(-1, 1) for d in dists]
-        dists = np.concatenate(dists, axis=1)  # b x c
-        scores = np.max(-dists, axis=1)  # b x 1, higher is better
-
-        # Distance score
-        # scores = torch.linalg.vector_norm(features, dim=1, ord=2)  # higher is better
+        cluster_scores = self._cluster_score(features)
+        dist_scores = self._dist_score(features)
+        
+        data = self._concat((cluster_scores, dist_scores))
+        combined_scores = self.data_kde.cdf(data)
 
         _, preds = torch.max(logits, dim=1)
-        return preds, torch.as_tensor(scores)
+        return preds, torch.as_tensor(combined_scores)
 
     def set_hyperparam(self, hyperparam: list):
         self.p = hyperparam[0]
