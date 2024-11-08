f13f9d115ebcf95edc1a54190d482a7df4ead5d9
Felix Hauser <felix.hauser@kit.edu>
2024-11-08 18:06:28 +0100

    Update run scripts

--- run0.bash
+++ run0.bash
@@ -1,22 +1,23 @@
 trap 'pkill -P $$; exit' SIGINT SIGTERM
 
-BENCHMARK=cifar100
+BENCHMARK=imagenet200
 
-CUDA_VISIBLE_DEVICES=0 python main_ood.py benchmark=$BENCHMARK postprocessor=msp &
-CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=mds &
-CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=odin &
+CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=msp &
+CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=mds &
+wait $(jobs -p)
+CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=odin &
+CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=knn &
 wait $(jobs -p)
 
-CUDA_VISIBLE_DEVICES=0 python main_ood.py benchmark=$BENCHMARK postprocessor=knn &
 CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=react &
 CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=dice &
 wait $(jobs -p)
-
-CUDA_VISIBLE_DEVICES=0 python main_ood.py benchmark=$BENCHMARK postprocessor=nusa &
-CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=vim &
+CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=nusa &
+CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=vim &
 wait $(jobs -p)
 
-CUDA_VISIBLE_DEVICES=0 python main_ood.py benchmark=$BENCHMARK postprocessor=ncscore &
-CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=neco &
-CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=epa &
+CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=ncscore &
+CUDA_VISIBLE_DEVICES=2 python main_ood.py benchmark=$BENCHMARK postprocessor=neco &
+wait $(jobs -p)
+CUDA_VISIBLE_DEVICES=1 python main_ood.py benchmark=$BENCHMARK postprocessor=epa &
 wait $(jobs -p)
