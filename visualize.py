bfa0a4cd510ab4963f4fd6d6cc03cda18300b83c
Felix Hauser <felix.hauser@kit.edu>
2024-11-09 21:48:40 +0100

    Add NC metric plot

--- visualize.py
+++ visualize.py
@@ -29,19 +29,6 @@ markers = [
     '_',  # Horizontal line
 ]
 
-nc_metrics = [
-    'nc1_strong',
-    'nc1_weak_between',
-    'nc1_weak_within',
-    'nc1_cdnv',
-    'nc2_equinormness',
-    'nc2_equiangularity',
-    'gnc2_hyperspherical_uniformity',
-    'nc3_self_duality',
-    'unc3_uniform_duality',
-    'nc4_classifier_agreement'
-]
-
 
 def plot_nc_ood(benchmark_name,
                 run_id,
@@ -93,5 +80,66 @@ def plot_nc_ood(benchmark_name,
     plt.close()
 
 
+def plot_nc(benchmark_name,
+            run_id):
+
+    main_dir = path.res_data / benchmark_name / run_id
+    ckpt_dirs = sorted(list(main_dir.glob('e*')))
+
+    epoch = []
+    nc = defaultdict(list)
+
+    for ckpt_dir in ckpt_dirs:
+        with pd.HDFStore(ckpt_dir / 'metrics.h5') as store:
+            epoch.append(int(re.search(r'e(\d+)$', ckpt_dir.name).group(1)))
+            nc_df = store.get('nc')
+            for name, value in nc_df.items():
+                nc[name].append(value)
+    
+    def plot_line(ax, values, label, marker):
+        ax.plot(values, label=label, marker=marker, markersize=5)
+    
+
+    fig, axes = plt.subplots(2, 2, figsize=(10, 8))
+
+    plot_line(axes[0, 0], nc['nc1_strong'], 'nc1_strong', markers[0])
+    plot_line(axes[0, 0], nc['nc1_weak_between'], 'nc1_weak_between', markers[1])
+    plot_line(axes[0, 0], nc['nc1_weak_within'], 'nc1_weak_within', markers[2])
+    plot_line(axes[0, 0], nc['nc1_cdnv'], 'nc1_cdnv', markers[3])
+
+    plot_line(axes[0, 1], nc['nc2_equinormness'], 'nc2_equinormness', markers[0])
+    plot_line(axes[0, 1], nc['nc2_equiangularity'], 'nc2_equiangularity', markers[1])
+    plot_line(axes[0, 1], nc['gnc2_hyperspherical_uniformity'], 'gnc2_hyperspherical_uniformity', markers[2])
+
+    plot_line(axes[1, 0], nc['nc3_self_duality'], 'nc3_self_duality', markers[0])
+    plot_line(axes[1, 0], nc['unc3_uniform_duality'], 'unc3_uniform_duality', markers[1])
+
+    plot_line(axes[1, 1], nc['nc4_classifier_agreement'], 'nc4_classifier_agreement', markers[0])
+        
+    axes[0, 0].legend()
+    axes[0, 1].legend()
+    axes[1, 0].legend()
+    axes[1, 1].legend()
+
+    axes[0, 0].set_title('NC1')
+    axes[0, 1].set_title('NC2')
+    axes[1, 0].set_title('NC3')
+    axes[1, 1].set_title('NC4')
+    
+    axes[0, 0].set_xlabel('epoch')
+    axes[0, 1].set_xlabel('epoch')
+    axes[1, 0].set_xlabel('epoch')
+    axes[1, 1].set_xlabel('epoch')
+
+    plt.tight_layout()
+
+    save_path = path.res_plots / benchmark_name / run_id
+    save_path.mkdir(exist_ok=True, parents=True)
+    filename = 'nc_all.png'
+    plt.savefig(save_path / filename)
+    plt.close()
+
+
 if __name__ == '__main__':
-    plot_nc_ood('cifar10', 'run0')
+    # plot_nc_ood('cifar10', 'run0')
+    plot_nc('cifar10', 'run0')
