b0999a5cc018851f63bbd12a02ee61f41c6431c5
Felix Hauser <felix.hauser@kit.edu>
2024-11-12 01:06:16 +0100

    Add script to run all visualizations

---
+++ show.bash
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+python visualize.py benchmark=cifar10 run=run0
+python visualize.py benchmark=cifar10 run=run1
+# python visualize.py benchmark=cifar10 run=run2
+python visualize.py benchmark=cifar10 run=run3
+python visualize.py benchmark=cifar10 run=run4
