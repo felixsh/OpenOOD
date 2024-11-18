22af0313e94766e26a9a1f512cc6fec6d3f042d7
Felix Hauser <felix.hauser@kit.edu>
2024-11-18 19:02:53 +0100

    Change path locations

--- path.py
+++ path.py
@@ -1,16 +1,21 @@
 from pathlib import Path
 
-
-res_data = Path('./res_data')
-res_data.mkdir(exist_ok=True, parents=True)
-
-res_plots = Path('./res_plots')
-res_plots.mkdir(exist_ok=True, parents=True)
-
-res_scores = Path('./res_scores')
-res_scores.mkdir(exist_ok=True, parents=True)
-
+# Loading
 data_root = Path('/home/hauser/data_openood')
 # data_root = Path('/storage_local/datasets/public/data_openood')
 
 ckpt_root = Path('/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks')
+
+
+# Saving
+# res_root = Path('.')
+res_root = Path('/mrtstorage/users/hauser/openood_res')
+
+res_data = res_root / 'data'
+res_data.mkdir(exist_ok=True, parents=True)
+
+res_plots = res_root / 'res_plots'
+res_plots.mkdir(exist_ok=True, parents=True)
+
+res_scores = res_root / 'res_scores'
+res_scores.mkdir(exist_ok=True, parents=True)
