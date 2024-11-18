8c52eb02a08f4e358b6bb416ba50f83c539756a5
Felix Hauser <felix.hauser@kit.edu>
2024-11-18 18:01:29 +0100

    Change checkpoint dir to mrtstorage

--- path.py
+++ path.py
@@ -12,4 +12,5 @@ res_scores.mkdir(exist_ok=True, parents=True)
 
 data_root = Path('/home/hauser/data_openood')
 # data_root = Path('/storage_local/datasets/public/data_openood')
-ckpt_root = Path('./checkpoints')
+
+ckpt_root = Path('/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks')
