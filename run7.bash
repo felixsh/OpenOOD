967b7c26f0a4d0d5f26bfb746ba68013415b8cd6
Felix Hauser <felix.hauser@kit.edu>
2024-11-18 15:47:42 +0100

    Update run script

--- run7.bash
+++ run7.bash
@@ -6,22 +6,17 @@ trap 'pkill -P $$; exit' SIGINT SIGTERM
 
 BENCHMARK=cifar10_noise
 
+#CUDA_VISIBLE_DEVICES=0 python eval_main.py benchmark=$BENCHMARK run=run0 &
+#CUDA_VISIBLE_DEVICES=1 python eval_main.py benchmark=$BENCHMARK run=run1 &
+#CUDA_VISIBLE_DEVICES=2 python eval_main.py benchmark=$BENCHMARK run=run2 &
+#wait $(jobs -p)
+
 CUDA_VISIBLE_DEVICES=0 python eval_main.py benchmark=$BENCHMARK run=run0 &
 CUDA_VISIBLE_DEVICES=1 python eval_main.py benchmark=$BENCHMARK run=run1 &
 CUDA_VISIBLE_DEVICES=2 python eval_main.py benchmark=$BENCHMARK run=run2 &
-wait $(jobs -p)
-
-CUDA_VISIBLE_DEVICES=0 python eval_main.py benchmark=$BENCHMARK run=run3 &
-CUDA_VISIBLE_DEVICES=1 python eval_main.py benchmark=$BENCHMARK run=run4 &
-CUDA_VISIBLE_DEVICES=2 python eval_main.py benchmark=$BENCHMARK run=run5 &
-wait $(jobs -p)
-
-CUDA_VISIBLE_DEVICES=0 python eval_main.py benchmark=$BENCHMARK run=run6 &
-CUDA_VISIBLE_DEVICES=1 python eval_main.py benchmark=$BENCHMARK run=run7 &
-CUDA_VISIBLE_DEVICES=2 python eval_main.py benchmark=$BENCHMARK run=run8 &
-wait $(jobs -p)
-
-CUDA_VISIBLE_DEVICES=0 python eval_main.py benchmark=$BENCHMARK run=run9 &
-CUDA_VISIBLE_DEVICES=1 python eval_main.py benchmark=$BENCHMARK run=run10 &
-CUDA_VISIBLE_DEVICES=2 python eval_main.py benchmark=$BENCHMARK run=run11 &
+CUDA_VISIBLE_DEVICES=3 python eval_main.py benchmark=$BENCHMARK run=run3 &
+CUDA_VISIBLE_DEVICES=4 python eval_main.py benchmark=$BENCHMARK run=run4 &
+CUDA_VISIBLE_DEVICES=5 python eval_main.py benchmark=$BENCHMARK run=run5 &
+CUDA_VISIBLE_DEVICES=6 python eval_main.py benchmark=$BENCHMARK run=run6 &
+CUDA_VISIBLE_DEVICES=7 python eval_main.py benchmark=$BENCHMARK run=run7 &
 wait $(jobs -p)
