a2659584de9ee84561934cbadf4a16867eea8eb7
Felix Hauser <felix.hauser@kit.edu>
2024-11-20 23:20:44 +0100

    Clean up paths

--- path.py
+++ path.py
@@ -14,10 +14,10 @@ res_root = Path('/mrtstorage/users/hauser/openood_res')
 res_data = res_root / 'data'
 res_data.mkdir(exist_ok=True, parents=True)
 
-res_plots = res_root / 'res_plots'
+res_plots = res_root / 'plots'
 res_plots.mkdir(exist_ok=True, parents=True)
 
-res_scores = res_root / 'res_scores'
+res_scores = res_root / 'scores'
 res_scores.mkdir(exist_ok=True, parents=True)
 
 cache_root = res_root / 'cache'
