b125c834990967a158112fcf9fc11ca2cddc79a8
Felix Hauser <felix.hauser@kit.edu>
2024-11-21 00:29:47 +0100

    Update run scripts

--- run4.bash
+++ run4.bash
@@ -6,7 +6,7 @@ trap 'pkill -P $$; exit' SIGINT SIGTERM
 
 #RUN0=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/200+_epochs/run_e200_2024_11_14-16_19_59
 #RUN1=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/400+_epochs/run_e400_2024_11_14-03_24_57
-RUN2=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/500+_epochs/run_e500_2024_11_12-22_24_32
+RUN2=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/150+_epochs/run_e150_2024_11_12-06_49_40
 
 OOD="[msp,odin,mds,react,dice,knn,nusa,vim,ncscore,neco,epa]"
 
