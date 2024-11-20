49693bde276067c6313c5381e02e3b42a500ce0a
Felix Hauser <felix.hauser@kit.edu>
2024-11-20 15:46:43 +0100

    Update run scripts

---
+++ run4.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# msp odin mds react dice knn nusa vim ncscore neco epa
+
+trap 'pkill -P $$; exit' SIGINT SIGTERM
+
+#RUN0=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/200+_epochs/run_e200_2024_11_14-16_19_59
+#RUN1=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/400+_epochs/run_e400_2024_11_14-03_24_57
+RUN2=/mrtstorage/users/truetsch/neural_collapse_runs/benchmarks/imagenet200/ResNet18_224x224/no_noise/500+_epochs/run_e500_2024_11_12-22_24_32
+
+OOD="[msp,odin,mds,react,dice,knn,nusa,vim,ncscore,neco,epa]"
+
+#CUDA_VISIBLE_DEVICES=0 python eval_main.py run=$RUN0 ood=$OOD &
+#CUDA_VISIBLE_DEVICES=1 python eval_main.py run=$RUN1 ood=$OOD &
+CUDA_VISIBLE_DEVICES=1 python eval_main.py run=$RUN2 ood=$OOD &
+wait $(jobs -p)
