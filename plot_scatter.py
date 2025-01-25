8ebad7a5b990687ea9bab46bfc2e41e83fe3bdee
Felix Hauser <felix.hauser@kit.edu>
2025-01-25 20:37:36 +0100

    Add new scatter plot

--- plot_scatter.py
+++ plot_scatter.py
@@ -57,18 +57,58 @@ def plot_scatter_all(benchmark_name, nc_split='val', ood_metric='AUROC'):
         nc_values = nc_dict[nc_metric]
         # Plot and save
         fig = _plot(acc, nc_values, _mean(nood_values, food_values), color_id, nc_metric, ood_metric, 'mean')
-        _save(fig, save_dir, f'mean_{nc_metric}_{ood_metric}')
-        fig = _plot(acc, nc_values, nood_values, color_id, nc_metric, ood_metric, 'near')
-        _save(fig, save_dir, f'scatter_near_{nc_metric}_{ood_metric}')
-        fig = _plot(acc, nc_values, food_values, color_id, nc_metric, ood_metric, 'far')
-        _save(fig, save_dir, f'scatter_far_{nc_metric}_{ood_metric}')
+        _save(fig, save_dir, f'scatter_mean_{nc_metric}_{ood_metric}')
+        # fig = _plot(acc, nc_values, nood_values, color_id, nc_metric, ood_metric, 'near')
+        # _save(fig, save_dir, f'scatter_near_{nc_metric}_{ood_metric}')
+        # fig = _plot(acc, nc_values, food_values, color_id, nc_metric, ood_metric, 'far')
+        # _save(fig, save_dir, f'scatter_far_{nc_metric}_{ood_metric}')
+
+
+def plot_scatter_tableau(benchmark_name, nc_split='val', ood_metric='AUROC'):
+    print(f'plotting scatter {benchmark_name} nc_{nc_split} ...')
+    _, epochs, acc, nc_dict, nood_dict, food_dict, save_dir = load_benchmark_data(benchmark_name, nc_split, ood_metric)
+
+    # Mean over ood methods
+    nood_values = list(np.mean([v for v in nood_dict.values()], axis=0))
+    food_values = list(np.mean([v for v in food_dict.values()], axis=0))
+    ood_values = _mean(nood_values, food_values)
+
+    # Epochs to color index
+    _, color_id = np.unique(epochs, return_inverse=True)
+    color_id += 1
+
+    save_dir = path.res_plots / 'scatter_tableau'
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    assert len(nc_dict.keys()) == len(nc_metrics)
+
+    nc_dict['acc val'] = acc
+
+    fig, axes = plt.subplots(3, 4, figsize=(12, 8))
+
+    for i, (k, v) in enumerate(nc_dict.items()):
+        axes.ravel()[i].scatter(v, ood_values, c=color_id, marker='o')
+        axes.ravel()[i].set_xlabel(k)
+        axes.ravel()[i].set_ylabel(ood_metric)
+
+    plt.tight_layout()
+
+    _save(fig, save_dir, f'scatter_tableau_{benchmark_name}')
 
 
 if __name__ == '__main__':
-    # plot_scatter_all('cifar10')
+    plot_scatter_all('cifar10')
     # plot_scatter_all('cifar100')
-    plot_scatter_all('imagenet200')
-    # plot_scatter_all('imagenet')
+    # plot_scatter_all('imagenet200')
+    plot_scatter_all('imagenet')
     # plot_scatter_all('alexnet')
     # plot_scatter_all('mobilenet')
     # plot_scatter_all('vgg')
+
+    plot_scatter_tableau('cifar10')
+    # plot_scatter_tableau('cifar100')
+    # plot_scatter_tableau('imagenet200')
+    plot_scatter_tableau('imagenet')
+    # plot_scatter_tableau('alexnet')
+    # plot_scatter_tableau('mobilenet')
+    # plot_scatter_tableau('vgg')
