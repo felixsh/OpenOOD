fc5809fe6190f1269bd29fbaae248cf67939897f
Felix Hauser <felix.hauser@kit.edu>
2025-01-27 18:54:44 +0100

    Update ood plotting

--- plot_ood.py
+++ plot_ood.py
@@ -10,24 +10,28 @@ from plot_utils import colors, metric_markers
 from plot_utils import load_acc, load_ood
 
 
-def _plot(save_path, filename, ood_keys, data, far, x_axis, acc_split, ood_metric):
+def _plot(save_path, filename, ood_keys, data, x_axis, acc_split, ood_metric):
     epochs, avg_acc, avg_near_ood, avg_far_ood = data
 
     # Plotting
     # plt.title(f'{benchmark_name} {"far" if far else "near"}')
-    for ood_key in ood_keys:
-        y_values = avg_far_ood[ood_key] if far else avg_near_ood[ood_key]
+    def sub_plot(ax, avg_ood, color):
+        for ood_key in ood_keys:
+            y_values = avg_ood[ood_key]
 
-        zipped =  zip(epochs, y_values) if x_axis == "epoch" else zip(avg_acc, y_values)
+            zipped =  zip(epochs, y_values) if x_axis == "epoch" else zip(avg_acc, y_values)
 
-        # Remove NaNs from data
-        valid_data = [(x, y) for x, y in zipped if not np.isnan(y)]
-        if not valid_data:
-            continue
-        x_values, y_values = zip(*valid_data)
-        plt.plot(x_values, y_values, '-', alpha=0.3, color=colors[1] if far else colors[0])
-        plt.plot(x_values, y_values, metric_markers[ood_key],
-                 color=colors[1] if far else colors[0], label=ood_key)
+            # Remove NaNs from data
+            valid_data = [(x, y) for x, y in zipped if not np.isnan(y)]
+            if not valid_data:
+                continue
+            x_values, y_values = zip(*valid_data)
+            ax.plot(x_values, y_values, '-', alpha=0.3, color=color)
+            ax.plot(x_values, y_values, metric_markers[ood_key], color=color, label=ood_key)
+    
+    _, axes = plt.subplots(1, 2, figsize=(15, 5))
+    sub_plot(axes[0], avg_near_ood, colors[0])
+    sub_plot(axes[1], avg_far_ood, colors[1])
 
     if x_axis == "epoch":
         plt.xlabel('epoch')
@@ -54,8 +58,6 @@ def plot_acc_ood_avg(benchmark_name,
     main_dirs = benchmark2loaddirs[benchmark_name]
     main_dirs = [Path(p) for p in main_dirs]
     run_dirs = [subdir for p in main_dirs if p.is_dir() for subdir in p.iterdir() if subdir.is_dir()]
-    save_dir = path.res_plots / main_dirs[0].relative_to(path.res_data).parents[-2]
-    save_dir.mkdir(parents=True, exist_ok=True)
 
     # Collect data
     acc_dict = defaultdict(list)
@@ -89,11 +91,19 @@ def plot_acc_ood_avg(benchmark_name,
     }
     data = (epochs, avg_acc, avg_near_ood, avg_far_ood)
 
-    _plot(save_dir, f'avg_{ood_metric}_near_acc_{acc_split}', ood_keys, data, False, None, acc_split, ood_metric)
-    _plot(save_dir, f'avg_{ood_metric}_far_acc_{acc_split}', ood_keys, data, True, None, acc_split, ood_metric)
-    _plot(save_dir, f'avg_{ood_metric}_near_epoch', ood_keys, data, False, 'epoch', acc_split, ood_metric)
-    _plot(save_dir, f'avg_{ood_metric}_far_epoch', ood_keys, data, True, 'epoch', acc_split, ood_metric)
+    save_dir = path.res_plots / 'ood'
+    save_dir.mkdir(parents=True, exist_ok=True)
+
+    _plot(save_dir, f'ood_{benchmark_name}_acc', ood_keys, data, None, acc_split, ood_metric)
+    _plot(save_dir, f'ood_{benchmark_name}_epoch', ood_keys, data, 'epoch', acc_split, ood_metric)
 
 
 if __name__ == '__main__':
     plot_acc_ood_avg('cifar10')
+    plot_acc_ood_avg('cifar100')
+    plot_acc_ood_avg('imagenet200')
+    plot_acc_ood_avg('imagenet')
+    plot_acc_ood_avg('noise')
+    plot_acc_ood_avg('alexnet')
+    plot_acc_ood_avg('vgg')
+    plot_acc_ood_avg('mobilenet')
