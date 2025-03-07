758d1bc3b3a81917654f05da7d7dc6b30ecebf29
Felix Hauser <felix.hauser@kit.edu>
2025-03-07 16:44:45 +0100

    Add train split to NC plots

--- plot_nc.py
+++ plot_nc.py
@@ -12,8 +12,10 @@ def _save_plot(fig, save_path, filename):
 
 
 def _plot_grid(
-    nc_mean,
-    nc_std,
+    nc_val_mean,
+    nc_val_std,
+    nc_train_mean,
+    nc_train_std,
     acc_val_mean,
     acc_val_std,
     acc_train_mean,
@@ -22,12 +24,27 @@ def _plot_grid(
     x_label,
     with_errorbars=False,
 ):
-    def _plot_line(ax, x, y, label, marker, color=None, error=None):
+    def _plot_line(
+        ax, x, y, label=None, marker=None, color=None, linestyle='-', error=None
+    ):
+        if linestyle != '-':
+            label = None
+
         if color is None:
             if with_errorbars and error is not None:
-                ax.errorbar(x, y, yerr=error, label=label, marker=marker, markersize=5)
+                ax.errorbar(
+                    x,
+                    y,
+                    yerr=error,
+                    label=label,
+                    marker=marker,
+                    markersize=5,
+                    linestyle=linestyle,
+                )
             else:
-                ax.plot(x, y, label=label, marker=marker, markersize=5)
+                ax.plot(
+                    x, y, label=label, marker=marker, markersize=5, linestyle=linestyle
+                )
         else:
             if with_errorbars and error is not None:
                 ax.errorbar(
@@ -38,102 +55,172 @@ def _plot_grid(
                     marker=marker,
                     markersize=5,
                     color=color,
+                    linestyle=linestyle,
                 )
             else:
-                ax.plot(x, y, label=label, marker=marker, markersize=5, color=color)
+                ax.plot(
+                    x,
+                    y,
+                    label=label,
+                    marker=marker,
+                    markersize=5,
+                    color=color,
+                    linestyle=linestyle,
+                )
 
     fig, axes = plt.subplots(2, 2, figsize=(10, 8))
 
-    # Subplot 00
+    # Subplot 00 ======================================================================================
     _plot_line(
         axes[0, 0],
         x,
-        nc_mean['nc1_weak_between'],
+        nc_val_mean['nc1_weak_between'],
         'nc1_weak_between',
         markers[1],
-        error=nc_std['nc1_weak_between'],
+        error=nc_val_std['nc1_weak_between'],
+        color=colors[0],
     )
     _plot_line(
         axes[0, 0],
         x,
-        nc_mean['nc1_weak_within'],
+        nc_train_mean['nc1_weak_between'],
+        linestyle='--',
+        color=colors[0],
+    )
+    _plot_line(
+        axes[0, 0],
+        x,
+        nc_val_mean['nc1_weak_within'],
         'nc1_weak_within',
         markers[2],
-        error=nc_std['nc1_weak_within'],
+        error=nc_val_std['nc1_weak_within'],
+        color=colors[1],
+    )
+    _plot_line(
+        axes[0, 0],
+        x,
+        nc_train_mean['nc1_weak_within'],
+        linestyle='--',
+        color=colors[1],
     )
     axes[0, 0].set_ylabel('nc1_weak')
     ax001 = axes[0, 0].twinx()
     _plot_line(
         ax001,
         x,
-        nc_mean['nc1_cdnv_mean'],
+        nc_val_mean['nc1_cdnv_mean'],
         'nc1_cdnv_mean',
         markers[3],
         color=colors[2],
-        error=nc_std['nc1_cdnv_mean'],
+        error=nc_val_std['nc1_cdnv_mean'],
+    )
+    _plot_line(
+        ax001,
+        x,
+        nc_train_mean['nc1_cdnv_mean'],
+        linestyle='--',
+        color=colors[2],
     )
     ax001.set_ylabel('nc1_cdnv_mean')
     # plot_line(ax001, x, nc['nc1_strong'], 'nc1_strong', markers[0], color=colors[3])
 
-    # Subplot 01
+    # Subplot 01 ======================================================================================
     _plot_line(
         axes[0, 1],
         x,
-        nc_mean['nc2_equinormness_mean'],
+        nc_val_mean['nc2_equinormness_mean'],
         'nc2_equinormness_mean',
         markers[0],
-        error=nc_std['nc2_equinormness_mean'],
+        error=nc_val_std['nc2_equinormness_mean'],
+        color=colors[0],
+    )
+    _plot_line(
+        axes[0, 1],
+        x,
+        nc_train_mean['nc2_equinormness_mean'],
+        'nc2_equinormness_mean',
+        linestyle='--',
+        color=colors[0],
     )
     axes[0, 1].set_ylabel('nc2_equinormness_mean')
     ax011 = axes[0, 1].twinx()
     _plot_line(
         ax011,
         x,
-        nc_mean['nc2_equiangularity_mean'],
+        nc_val_mean['nc2_equiangularity_mean'],
         'nc2_equiangularity_mean',
         markers[1],
         color=colors[1],
-        error=nc_std['nc2_equiangularity_mean'],
+        error=nc_val_std['nc2_equiangularity_mean'],
+    )
+    _plot_line(
+        ax011,
+        x,
+        nc_train_mean['nc2_equiangularity_mean'],
+        linestyle='--',
+        color=colors[1],
     )
     ax011.set_ylabel('nc2_equiangularity_mean')
     # plot_line(axes[0, 1], x, nc['gnc2_hyperspherical_uniformity'], 'gnc2_hyperspherical_uniformity', markers[2])
 
-    # Subplot 10
+    # Subplot 10 ======================================================================================
     _plot_line(
         axes[1, 0],
         x,
-        nc_mean['nc3_self_duality'],
+        nc_val_mean['nc3_self_duality'],
         'nc3_self_duality',
         markers[0],
-        error=nc_std['nc3_self_duality'],
+        error=nc_val_std['nc3_self_duality'],
+        color=colors[0],
+    )
+    _plot_line(
+        axes[1, 0],
+        x,
+        nc_train_mean['nc3_self_duality'],
+        linestyle='--',
+        color=colors[0],
     )
     ax101 = axes[1, 0].twinx()
     _plot_line(
         ax101,
         x,
-        nc_mean['unc3_uniform_duality_mean'],
+        nc_val_mean['unc3_uniform_duality_mean'],
         'unc3_uniform_duality_mean',
         markers[1],
         color=colors[1],
-        error=nc_std['unc3_uniform_duality_mean'],
+        error=nc_val_std['unc3_uniform_duality_mean'],
+    )
+    _plot_line(
+        ax101,
+        x,
+        nc_train_mean['unc3_uniform_duality_mean'],
+        linestyle='--',
+        color=colors[1],
     )
     ax101.set_ylabel('unc3_mean')
     axes[1, 0].set_ylabel('nc3')
 
-    # Subplot 11
+    # Subplot 11 ======================================================================================
     _plot_line(axes[1, 1], x, acc_train_mean, 'acc_train', 'None', error=acc_train_std)
     _plot_line(axes[1, 1], x, acc_val_mean, 'acc_val', 'None', error=acc_val_std)
     _plot_line(
         axes[1, 1],
         x,
-        nc_mean['nc4_classifier_agreement'],
+        nc_val_mean['nc4_classifier_agreement'],
         'nc4_classifier_agreement',
         markers[0],
-        error=nc_std['nc4_classifier_agreement'],
+        error=nc_val_std['nc4_classifier_agreement'],
+    )
+    _plot_line(
+        axes[1, 1],
+        x,
+        nc_train_mean['nc4_classifier_agreement'],
+        linestyle='--',
+        color=colors[2],
     )
     axes[1, 1].set_ylabel('agreement/accuracy')
 
-    # Legend subplot 00
+    # Legend subplot 00 ======================================================================================
     lines000, labels000 = axes[0, 0].get_legend_handles_labels()
     lines001, labels001 = ax001.get_legend_handles_labels()
     ax001.legend(lines000 + lines001, labels000 + labels001)
@@ -170,12 +257,16 @@ def tolerant_mean(arrs):
 
 
 def plot_nc(benchmark_name):
-    _, epochs, acc_val, acc_train, nc, _, _, _ = load_benchmark_data(benchmark_name)
+    _, epochs, acc_val, acc_train, nc_val, _, _ = load_benchmark_data(
+        benchmark_name, nc_split='val'
+    )
+
+    _, _, _, _, nc_train, _, _ = load_benchmark_data(benchmark_name, nc_split='train')
 
     if benchmark_name == 'imagenet':
         with_errorbars = False
-        nc_mean = nc
-        nc_std = nc
+        nc_val_mean = nc_val
+        nc_val_std = nc_val
         acc_val_mean = acc_val
         acc_val_std = acc_val
         acc_train_mean = acc_train
@@ -191,13 +282,22 @@ def plot_nc(benchmark_name):
         acc_train_splits = np.split(acc_train, indices_or_sections=idx)
         acc_train_mean, acc_train_std = tolerant_mean(acc_train_splits)
 
-        for k, v in nc.items():
-            nc[k] = np.split(v, indices_or_sections=idx)
+        nc_val_split = {}
+        for k, v in nc_val.items():
+            nc_val_split[k] = np.split(v, indices_or_sections=idx)
+        nc_train_split = {}
+        for k, v in nc_train.items():
+            nc_train_split[k] = np.split(v, indices_or_sections=idx)
 
-        nc_mean = {}
-        nc_std = {}
-        for k, v in nc.items():
-            nc_mean[k], nc_std[k] = tolerant_mean(v)
+        nc_val_mean = {}
+        nc_val_std = {}
+        for k, v in nc_val_split.items():
+            nc_val_mean[k], nc_val_std[k] = tolerant_mean(v)
+
+        nc_train_mean = {}
+        nc_train_std = {}
+        for k, v in nc_train_split.items():
+            nc_train_mean[k], nc_train_std[k] = tolerant_mean(v)
 
         epochs, _ = tolerant_mean(epochs)
 
@@ -207,8 +307,10 @@ def plot_nc(benchmark_name):
     save_dir.mkdir(exist_ok=True, parents=True)
 
     fig = _plot_grid(
-        nc_mean,
-        nc_std,
+        nc_val_mean,
+        nc_val_std,
+        nc_train_mean,
+        nc_train_std,
         acc_val_mean,
         acc_val_std,
         acc_train_mean,
@@ -220,8 +322,10 @@ def plot_nc(benchmark_name):
     _save_plot(fig, save_dir, f'nc_{benchmark_name}_epoch')
 
     fig = _plot_grid(
-        nc_mean,
-        nc_std,
+        nc_val_mean,
+        nc_val_std,
+        nc_train_mean,
+        nc_train_std,
         acc_val_mean,
         acc_val_std,
         acc_train_mean,
@@ -234,10 +338,10 @@ def plot_nc(benchmark_name):
 
 
 if __name__ == '__main__':
-    plot_nc('cifar10')
+    # plot_nc('cifar10')
     plot_nc('cifar100')
-    plot_nc('imagenet200')
-    plot_nc('imagenet')
-    plot_nc('alexnet')
-    plot_nc('vgg')
-    plot_nc('mobilenet')
+    # plot_nc('imagenet200')
+    # plot_nc('imagenet')
+    # plot_nc('alexnet')
+    # plot_nc('vgg')
+    # plot_nc('mobilenet')
