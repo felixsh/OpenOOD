1d7dc199a413521e12d0fcc22be48113fe832b2e
Felix Hauser <felix.hauser@kit.edu>
2025-03-07 16:41:10 +0100

    Avoid url lookup issue in get_postprocessor()

--- openood/evaluation_api/postprocessor.py
+++ openood/evaluation_api/postprocessor.py
@@ -1,25 +1,56 @@
 import os
-import urllib.request
 
 from openood.postprocessors import (
-    ASHPostprocessor, BasePostprocessor, ConfBranchPostprocessor,
-    CutPastePostprocessor, DICEPostprocessor, DRAEMPostprocessor,
-    DropoutPostProcessor, DSVDDPostprocessor, EBOPostprocessor,
-    EnsemblePostprocessor, GMMPostprocessor, GodinPostprocessor,
-    GradNormPostprocessor, GRAMPostprocessor, KLMatchingPostprocessor,
-    KNNPostprocessor, MaxLogitPostprocessor, MCDPostprocessor,
-    MDSPostprocessor, MDSEnsemblePostprocessor, MOSPostprocessor,
-    ODINPostprocessor, OpenGanPostprocessor, OpenMax, PatchcorePostprocessor,
-    Rd4adPostprocessor, ReactPostprocessor, ResidualPostprocessor,
-    ScalePostprocessor, SSDPostprocessor, TemperatureScalingPostprocessor,
-    VIMPostprocessor, RotPredPostprocessor, RankFeatPostprocessor,
-    RMDSPostprocessor, SHEPostprocessor, CIDERPostprocessor, NPOSPostprocessor,
-    GENPostprocessor, NNGuidePostprocessor, RelationPostprocessor,
-    T2FNormPostprocessor, ReweightOODPostprocessor, fDBDPostprocessor,
-    NCScorePostprocessor, NECOPostprocessor,
-    EPAPostprocessor, NuSAPostprocessor,
+    ASHPostprocessor,
+    BasePostprocessor,
+    CIDERPostprocessor,
+    ConfBranchPostprocessor,
+    CutPastePostprocessor,
+    DICEPostprocessor,
+    DRAEMPostprocessor,
+    DropoutPostProcessor,
+    DSVDDPostprocessor,
+    EBOPostprocessor,
+    EnsemblePostprocessor,
+    EPAPostprocessor,
+    GENPostprocessor,
+    GMMPostprocessor,
+    GodinPostprocessor,
+    GradNormPostprocessor,
+    GRAMPostprocessor,
+    KLMatchingPostprocessor,
+    KNNPostprocessor,
+    MaxLogitPostprocessor,
+    MCDPostprocessor,
+    MDSEnsemblePostprocessor,
+    MDSPostprocessor,
+    MOSPostprocessor,
     MyPostprocessor,
-    )
+    NCScorePostprocessor,
+    NECOPostprocessor,
+    NNGuidePostprocessor,
+    NPOSPostprocessor,
+    NuSAPostprocessor,
+    ODINPostprocessor,
+    OpenGanPostprocessor,
+    OpenMax,
+    PatchcorePostprocessor,
+    RankFeatPostprocessor,
+    Rd4adPostprocessor,
+    ReactPostprocessor,
+    RelationPostprocessor,
+    ResidualPostprocessor,
+    ReweightOODPostprocessor,
+    RMDSPostprocessor,
+    RotPredPostprocessor,
+    ScalePostprocessor,
+    SHEPostprocessor,
+    SSDPostprocessor,
+    T2FNormPostprocessor,
+    TemperatureScalingPostprocessor,
+    VIMPostprocessor,
+    fDBDPostprocessor,
+)
 from openood.utils.config import Config, merge_configs
 
 postprocessors = {
@@ -41,7 +72,6 @@ postprocessors = {
     'vim': VIMPostprocessor,
     'gradnorm': GradNormPostprocessor,
     'godin': GodinPostprocessor,
-    'mds': MDSPostprocessor,
     'gram': GRAMPostprocessor,
     'cutpaste': CutPastePostprocessor,
     'mls': MaxLogitPostprocessor,
@@ -75,23 +105,23 @@ postprocessors = {
     'my': MyPostprocessor,
 }
 
-link_prefix = 'https://raw.githubusercontent.com/Jingkang50/OpenOOD/main/configs/postprocessors/'
+link_prefix = (
+    'https://raw.githubusercontent.com/Jingkang50/OpenOOD/main/configs/postprocessors/'
+)
 
 
-def get_postprocessor(config_root: str, postprocessor_name: str,
-                      id_data_name: str):
-    postprocessor_config_path = os.path.join(config_root, 'postprocessors',
-                                             f'{postprocessor_name}.yml')
+def get_postprocessor(config_root: str, postprocessor_name: str, id_data_name: str):
+    postprocessor_config_path = os.path.join(
+        config_root, 'postprocessors', f'{postprocessor_name}.yml'
+    )
     if not os.path.exists(postprocessor_config_path):
-        os.makedirs(os.path.dirname(postprocessor_config_path), exist_ok=True)
-        urllib.request.urlretrieve(link_prefix + f'{postprocessor_name}.yml',
-                                   postprocessor_config_path)
+        raise 'WHYNOTTTT???'
+        # os.makedirs(os.path.dirname(postprocessor_config_path), exist_ok=True)
+        # urllib.request.urlretrieve(link_prefix + f'{postprocessor_name}.yml',
+        #                            postprocessor_config_path)
 
     config = Config(postprocessor_config_path)
-    config = merge_configs(config,
-                           Config(**{'dataset': {
-                               'name': id_data_name
-                           }}))
+    config = merge_configs(config, Config(**{'dataset': {'name': id_data_name}}))
     postprocessor = postprocessors[postprocessor_name](config)
     postprocessor.APS_mode = config.postprocessor.APS_mode
     postprocessor.hyperparam_search_done = False
