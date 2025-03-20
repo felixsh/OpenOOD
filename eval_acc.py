b505aa0cb8e83fae80f59ca883ca243d4a6351f2
Felix Hauser <felix.hauser@kit.edu>
2025-03-20 14:40:25 +0100

    Big clean up, delete old stuff

--- eval_acc.py
+++ eval_acc.py
@@ -1,117 +1,8 @@
 from pathlib import Path
 
 import numpy as np
-import pandas as pd
-import torch
-from torch.utils.data import DataLoader
-from torchmetrics.classification import MulticlassAccuracy
-from tqdm import tqdm
 
-import path
 from feature_cache import FeatureCache
-from openood.evaluation_api.datasets import data_setup, get_id_ood_dataloader
-from openood.evaluation_api.preprocessor import get_default_preprocessor
-from utils import get_batch_size, get_benchmark_name, load_network
-
-
-class EvalAcc(object):
-    def __init__(self, benchmark_name, ckpt_path) -> None:
-        self.benchmark_name = benchmark_name
-        self.ckpt_path = ckpt_path
-
-        # Parameters
-        batch_size = get_batch_size(benchmark_name)
-        shuffle = False
-        num_workers = 1
-
-        # Prepare data
-        data_root = str(path.data_root)
-        preprocessor = get_default_preprocessor(benchmark_name)
-        data_setup(data_root, benchmark_name)
-        loader_kwargs = {
-            'batch_size': batch_size,
-            'shuffle': shuffle,
-            'num_workers': num_workers,
-        }
-        self.dataloader_dict = get_id_ood_dataloader(
-            benchmark_name, data_root, preprocessor, **loader_kwargs
-        )
-
-        # Load model
-        self.net = load_network(benchmark_name, ckpt_path)
-
-        # Get features, labels, logits, predictions
-        exampel_batch = next(iter(self.dataloader_dict['id']['train']))
-        exampel_inp = exampel_batch['data'].cuda().float()
-
-        example_out = self.net(exampel_inp, return_feature=False)
-
-        self.num_classes = example_out.shape[1]
-
-    def compute(self, dataloader):
-        accuracy = MulticlassAccuracy(num_classes=self.num_classes).cuda()
-
-        with torch.no_grad():
-            for batch in tqdm(dataloader):
-                inp = batch['data'].cuda().float()
-                out = self.net(inp, return_feature=False)
-
-                labels = batch['label'].cuda()
-                preds = out.argmax(dim=1)
-                accuracy.update(preds, labels)
-
-        return accuracy.compute().item()
-
-    def walk_(self, d: dict) -> dict:
-        """Recursively walks through a dictionary and evaluate DataLoader objects."""
-        result = {}
-        for key, value in d.items():
-            if isinstance(value, DataLoader):
-                result[key] = self.compute(value)
-            elif isinstance(value, dict):
-                result[key] = self.walk_(value)
-            else:
-                result[key] = None  # Placeholder for non-DataLoader items
-        return result
-
-    def walk(self):
-        return self.walk_(self.dataloader_dict)
-
-    def eval(self, split):
-        assert split in ['train', 'val']
-        acc = self.compute(self.dataloader_dict['id'][split])
-        res = {'id': {split: acc}}
-        return res
-
-
-def flatten_dict(nested_dict):
-    res = {}
-    if isinstance(nested_dict, dict):
-        for k in nested_dict:
-            flattened_dict = flatten_dict(nested_dict[k])
-            for key, val in flattened_dict.items():
-                key = list(key)
-                key.insert(0, k)
-                res[tuple(key)] = val
-    else:
-        res[()] = nested_dict
-    return res
-
-
-def nested_dict_to_df(values_dict):
-    flat_dict = flatten_dict(values_dict)
-    df = pd.DataFrame.from_dict(flat_dict, orient='index')
-    df.index = pd.MultiIndex.from_tuples(df.index)
-    df = df.unstack(level=-1)
-    df.columns = df.columns.map('{0[1]}'.format)
-    return df
-
-
-def eval_acc(ckpt_path, split='train'):
-    benchmark_name = get_benchmark_name(ckpt_path)
-    evaluator = EvalAcc(benchmark_name, Path(ckpt_path))
-    res = evaluator.eval(split)
-    return nested_dict_to_df(res)
 
 
 def eval_acc_cache(feature_cache: FeatureCache, split: str = 'train') -> float:
@@ -121,6 +12,7 @@ def eval_acc_cache(feature_cache: FeatureCache, split: str = 'train') -> float:
 
 
 if __name__ == '__main__':
+    # Small test
     from feature_cache import FeatureCache
 
     benchmark = 'cifar10'
--- eval_main.py
+++ eval_main.py
@@ -1,18 +1,12 @@
-import json
 import re
-from datetime import timedelta
 from pathlib import Path
-from timeit import default_timer as timer
 
-import torch.multiprocessing as mp
 from natsort import natsorted
-from omegaconf import OmegaConf
-from pandas import HDFStore
 
 import database
 import path
 import utils
-from eval_acc import eval_acc, eval_acc_cache
+from eval_acc import eval_acc_cache
 from eval_nc import eval_nc
 from eval_ood import eval_ood
 from feature_cache import FeatureCache
@@ -37,99 +31,6 @@ postprocessors = [
 # ["openmax", "msp", "temp_scaling", "odin", "mds", "mds_ensemble", "rmds", "gram", "ebo", "gradnorm", "react", "mls", "klm", "vim", "knn", "dice", "rankfeat", "ash", "she"]
 
 
-def save_ood(df, save_dir, filename, key):
-    # Store in HDF5 format
-    full_path = save_dir / f'{filename}.h5'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with HDFStore(full_path) as store:
-            store.put(key, df)
-
-    # Print markdown table to file
-    full_path = save_dir / f'{filename}.md'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with open(full_path, 'a') as f:
-            f.write(f'---\n{key}\n')
-            f.write(df.to_markdown())
-            f.write('\n')
-
-
-def save_nc(df, save_dir, filename, key):
-    # Store in HDF5 format
-    full_path = save_dir / f'{filename}.h5'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with HDFStore(full_path) as store:
-            store.put(key, df)
-
-    # Print markdown table to file
-    full_path = save_dir / f'{filename}.md'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with open(full_path, 'a') as f:
-            f.write(f'---\n{key}\n')
-            f.write(df.transpose().to_markdown())
-            f.write('\n')
-
-
-def save_acc(df, save_dir, filename):
-    key = 'acc'
-
-    # Store in HDF5 format
-    full_path = save_dir / f'{filename}.h5'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with HDFStore(full_path) as store:
-            store.put(key, df)
-
-    # Print markdown table to file
-    full_path = save_dir / f'{filename}.md'
-    lock = utils.get_lockfile(full_path)
-    with lock:
-        with open(full_path, 'a') as f:
-            f.write(f'---\n{key}\n')
-            f.write(df.transpose().to_markdown())
-            f.write('\n')
-
-
-def save_scores(score_dict, save_dir, filename):
-    save_dir.mkdir(exist_ok=True, parents=True)
-
-    new_dict = {
-        'id': score_dict['id'],
-        'ood': score_dict['ood'],
-    }
-    converted_dict = utils.convert_numpy_to_lists(new_dict)
-
-    with open(save_dir / f'{filename}.json', 'w') as f:
-        json.dump(converted_dict, f, indent=4)
-
-
-def existing_keys(save_dir, filename):
-    full_path = save_dir / f'{filename}.h5'
-    lock = utils.get_lockfile(full_path)
-
-    with lock:
-        if full_path.is_file():
-            with HDFStore(full_path, mode='r') as store:
-                key_list = list(store.keys())
-
-            # Remove '/' from beginning of str key
-            key_list = [k[1:] for k in key_list]
-
-            # Remove old 'nc' key if present, only use new 'nc_train', 'nc_val' keys
-            try:
-                key_list.remove('nc')
-            except ValueError:
-                pass
-
-            return key_list
-
-        else:
-            return []
-
-
 def filtering_ckpts(
     ckpt_list, filter_list=[1, 2, 5, 10, 20, 50, 100, 200, 500, 1000, 2000, 5000]
 ):
@@ -139,43 +40,6 @@ def filtering_ckpts(
     ckpts_filtered.append(ckpt_list[-1])
     ckpts_filtered = natsorted(list(set(ckpts_filtered)), key=str)
     return ckpts_filtered
-
-
-def eval_run(run_dir, ood_method_list=postprocessors):
-    run_dir = Path(run_dir)
-    ckpt_list = get_run_ckpts(run_dir)
-
-    benchmark_name = utils.get_benchmark_name(run_dir)
-    print('BENCHMARK', benchmark_name)
-    save_dir = path.res_data / run_dir.relative_to(path.ckpt_root)
-    save_dir.mkdir(exist_ok=True, parents=True)
-
-    for ckpt_path in ckpt_list:
-        start_time = timer()
-        eval_ckpt_nc(benchmark_name, ckpt_path, save_dir)
-        eval_ckpt_ood(benchmark_name, ckpt_path, save_dir, list(ood_method_list))
-        eval_ckpt_acc(ckpt_path)
-        print(
-            f'{"\033[91m"}Checkpoint took {timedelta(seconds=timer() - start_time)}{"\033[0m"}'
-        )
-
-
-def eval_ckpt_nc(benchmark_name, ckpt_path, save_dir, recompute=False):
-    file_name = utils.get_epoch_name(ckpt_path)
-
-    done_keys = existing_keys(save_dir, file_name)
-    all_done = all([k in done_keys for k in ['nc_train', 'nc_val']])
-
-    if not all_done or recompute:
-        feature_cache = FeatureCache(benchmark_name, ckpt_path)
-
-        if 'nc_train' not in done_keys or recompute:
-            nc_metrics = eval_nc(feature_cache, split='train')
-            save_nc(nc_metrics, save_dir, file_name, 'nc_train')
-
-        if 'nc_val' not in done_keys or recompute:
-            nc_metrics = eval_nc(feature_cache, split='val')
-            save_nc(nc_metrics, save_dir, file_name, 'nc_val')
 
 
 def eval_ckpt_acc_nc(benchmark_name: str, ckpt_path: Path) -> None:
@@ -224,14 +88,6 @@ def eval_ckpt_ood(
         )
 
 
-def eval_ckpt_acc(ckpt_path):
-    file_name = utils.get_epoch_name(ckpt_path)
-    acc_metrics = eval_acc(ckpt_path)
-    save_dir = path.res_data / ckpt_path.parent.relative_to(path.ckpt_root)
-    save_dir.mkdir(exist_ok=True, parents=True)
-    save_acc(acc_metrics, save_dir, file_name)
-
-
 def get_run_ckpts(run_dir, filtering=True):
     run_dir = Path(run_dir)
     ckpt_list = [p for p in run_dir.glob('*') if p.suffix in ckpt_suffixes]
@@ -246,31 +102,3 @@ def get_previous_ckpts():
     ckpt_list = [path.ckpt_root / p.relative_to(path.cache_root) for p in cache_list]
     ckpt_list = [Path(re.sub('_train.npz$', '.pth', str(p))) for p in ckpt_list]
     return natsorted(ckpt_list, key=str)
-
-
-def recompute_all(ood_method_list=postprocessors):
-    for ckpt_path in get_previous_ckpts():
-        print(ckpt_path)
-        benchmark_name = utils.get_benchmark_name(ckpt_path)
-        save_dir = path.res_data / ckpt_path.parent.relative_to(path.ckpt_root)
-        save_dir.mkdir(exist_ok=True, parents=True)
-
-        eval_ckpt_ood(
-            benchmark_name, ckpt_path, save_dir, ood_method_list, recompute=True
-        )
-
-
-if __name__ == '__main__':
-    # https://stackoverflow.com/q/64654838
-    # mp.set_start_method('spawn', force=True)
-    mp.set_start_method('fork', force=True)
-
-    # main_cfg = OmegaConf.load('cfg/main.yaml')
-    main_cfg = OmegaConf.create({'recompute': False})
-    cli_cfg = OmegaConf.from_cli()
-    cfg = OmegaConf.merge(main_cfg, cli_cfg)
-
-    if cfg.recompute:
-        recompute_all(ood_method_list=[])
-    else:
-        eval_run(cfg.run, cfg.ood)
